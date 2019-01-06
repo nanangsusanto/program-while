8ec06f13d3cb5cf6ca1856efb26613da711021c5
nanangsusanto <46389404+nanangsusanto@users.noreply.github.com>
2019-01-05 23:50:32 -0800

    Add files via upload

---
+++ program while/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+	int a = 1;
+
+	while (a < 15 ){
+		cout << "TUGAS ALGORITMAC++\n";
+		cout << a << endl;
+		a += 3;
+	}
+	cout << "\n SEMANGAT ";
+
+	cin.get();
+	return 0;
+}
